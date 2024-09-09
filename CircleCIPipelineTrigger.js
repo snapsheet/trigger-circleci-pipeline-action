710adab506cb332dc77dbea5b0df13fd79773c17
Rohin Paikattil <paikattilrohin@gmail.com>
2024-09-09 00:39:55 -0500

    Remove logs

--- CircleCIPipelineTrigger.js
+++ CircleCIPipelineTrigger.js
@@ -141,9 +141,7 @@ export class CircleCIPipelineTrigger {
                 console.log("Trying to get workflow status");
                 const response = await axios.get(url, { headers });
                 console.log("Got response");
-                console.log(response);
                 const status = response.data.items[0].status;
-                console.log(status);
 
                 if (!["not_run", "on_hold", "running"].includes(status)) {
                     console.log("Inside if");
--- dist/index.js
+++ dist/index.js
@@ -459,9 +459,7 @@ class CircleCIPipelineTrigger {
                 console.log("Trying to get workflow status");
                 const response = await axios_default().get(url, { headers });
                 console.log("Got response");
-                console.log(response);
                 const status = response.data.items[0].status;
-                console.log(status);
 
                 if (!["not_run", "on_hold", "running"].includes(status)) {
                     console.log("Inside if");
